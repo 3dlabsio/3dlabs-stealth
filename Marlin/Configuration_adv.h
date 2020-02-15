b32f9020e1d3440df9bcd89bbb7661f754db8299
Shane Warner <shane@DESKTOP-CNL13KC.localdomain>
2020-02-15 13:50:49 -0800

    lower block buffer size to free additional sram

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -904,7 +904,7 @@
 #if ENABLED(SDSUPPORT)
   #define BLOCK_BUFFER_SIZE 16 // SD,LCD,Buttons take more memory, block buffer needs to be smaller
 #else
-  #define BLOCK_BUFFER_SIZE 16 // maximize block buffer
+  #define BLOCK_BUFFER_SIZE 8 // maximize block buffer
 #endif
 
 // @section serial
